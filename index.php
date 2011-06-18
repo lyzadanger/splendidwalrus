429b0ab290d99d7b762a62c410d15c02347caf56
Lyza Danger Gardner <lyza@lyza.com>
2011-06-18 12:46:59 -0700

    First set of split-out CSS and basic content

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+  <title>The Splendid Walrus: Choose Your Adventure</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+</head>
+<body>
+<h1>Choose your walrus adventure!</h1>
+<ul>
+  <li><a href="content.php?step=1">Step 1: Fixed Desktop Grid</a></li>
+  <li><a href="content.php?step=2">Step 2: Fixed Desktop Grid; CSS Factoring</a></li>
+  <li><a href="content.php?step=3">Step 3: Fixed Mobile and Desktop Grids</a></li>
+  <li><a href="content.php?step=4">Step 4: Fluid Desktop Grid</a></li>
+  <li><a href="content.php?step=5">Step 5: Fluid Both Grids</a></li>
+</ul>
+</body>
+</html>
